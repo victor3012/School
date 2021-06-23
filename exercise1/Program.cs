8f4ff6662dbf9709ea595e5b55fd7d6ab2a917f9
BuildTools <unconfigured@null.spigotmc.org>
2021-06-23 23:48:03 +0300

    Initial Commit

---
+++ exercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input - array's length
+            int n = Convert.ToInt32(Console.ReadLine());
+            // Declaring the array (with "n" elements)
+            int[] array = new int[n];
+            // Filling the array
+            for (int i = 0; i < n; i++)
+            {
+                // for each line -> filling the array's element ("i")
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Creating a sum variable (will be used to store the total sum)
+            int sum = 0;
+
+            // Going through every element in the array
+            foreach (int element in array)
+            {
+                // Check: if the number is even ( % 2 == 0)
+                if (element % 2 == 0)
+                {
+                    // If it is even -> adding its squared value to the sum
+                    sum += element * element;
+                }
+            }
+            
+            // Priting the sum 
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ pagination/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace pagination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] array =
+            {
+                "University", "Java", "JavaScript", "PHP", "HTML",
+                "CSS", "Spring", "Vue", "Angular", "CSS", "CSS"
+            };
+
+            int wordsPerPage = Convert.ToInt32(Console.ReadLine());
+            int selectedPage = Convert.ToInt32(Console.ReadLine());
+            String searchingPhrase = Console.ReadLine();
+
+            Boolean hasFound = false;
+            for (int i = (selectedPage - 1) * wordsPerPage; i < selectedPage * wordsPerPage; i++)
+            {
+                if (array[i].Contains(searchingPhrase))
+                {
+                    Console.WriteLine("Found this element: " + array[i]);
+                    hasFound = true;
+                }
+            }
+
+            if (!hasFound)
+            {
+                Console.WriteLine("No result.");
+            }
+            int totalElements = array.Length;
+            Console.WriteLine("Total Items: " +totalElements);
+            int pages = Convert.ToInt32(Math.Floor(totalElements * 1.0 / wordsPerPage));
+            Console.WriteLine("Total Pages: " +pages);
+        }
+    }
+}
